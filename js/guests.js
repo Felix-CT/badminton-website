763d62119655a6593162c1b829588dfbec131b94
Xeeto <pewfacepew@gmail.com>
2024-09-02 16:08:57 -0400

    added extra message on guests page

--- js/guests.js
+++ js/guests.js
@@ -37,6 +37,15 @@ var getGuestLimit = async () => {
 document.addEventListener('DOMContentLoaded', async function() {
     const guestLimit = await getGuestLimit();
     if (guestLimit !== null) {
-        document.getElementById("guests-count").innerText = guestLimit;
+        document.getElementById("guests-count").innerText = 'Current guest limit is ' + guestLimit;
+        console.log(guestLimit)
+        const guestsMessage = document.getElementById("guests-message")
+        if (guestLimit > 0) {
+            guestsMessage.innerText = "There is space available! Please message bellbclub@gmail.com to join us this week.";
+            guestsMessage.style.color = 'green'
+        } else {
+            guestsMessage.innerText = "Unfortunately there are no more spots available. Please check again later as spots may open up.\n A total of two guests are allowed per week."
+            guestsMessage.style.color = 'red'
+        }
     }
 });
