c39b3809368877859f0bb5c9137e5d53d6ab3961
Xeeto <pewfacepew@gmail.com>
2024-12-04 12:17:41 -0500

    check every second if the data is loaded

--- js/guests.js
+++ js/guests.js
@@ -1,16 +1,21 @@
+import { setTimeout } from "timers/promises";
+
 // Code to retrieve value from the database
 document.addEventListener('DOMContentLoaded', async function() {
-    const guestLimit = sessionStorage.getItem('guestLimit');
-    if (guestLimit) {
-        document.getElementById("guests-count").innerText = 'Current guest limit is ' + guestLimit;
-        console.log(guestLimit)
-        const guestsMessage = document.getElementById("guests-message")
-        if (guestLimit > 0) {
-            guestsMessage.innerText = "There is space available! Please message bellbclub@gmail.com to join us this week.";
-            guestsMessage.style.color = 'green'
-        } else {
-            guestsMessage.innerText = "Unfortunately there are no more spots available. Please check again later as spots may open up.\n A total of two guests are allowed per week."
-            guestsMessage.style.color = 'red'
-        }
+    let guestLimit = sessionStorage.getItem('guestLimit');
+    while (!guestLimit){
+        await setTimeout(1000);
+        guestLimit = sessionStorage.getItem('guestLimit')
+    }
+
+    document.getElementById("guests-count").innerText = 'Current guest limit is ' + guestLimit;
+    console.log(guestLimit)
+    const guestsMessage = document.getElementById("guests-message")
+    if (guestLimit > 0) {
+        guestsMessage.innerText = "There is space available! Please message bellbclub@gmail.com to join us this week.";
+        guestsMessage.style.color = 'green'
+    } else {
+        guestsMessage.innerText = "Unfortunately there are no more spots available. Please check again later as spots may open up.\n A total of two guests are allowed per week."
+        guestsMessage.style.color = 'red'
     }
 });
