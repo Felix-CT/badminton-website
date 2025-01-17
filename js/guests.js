0168d260e79c197c7923528edf36a2c3a10e52c2
Xeeto <pewfacepew@gmail.com>
2025-01-16 22:06:34 -0500

    updated the favicon

--- js/guests.js
+++ js/guests.js
@@ -1,22 +1,16 @@
-// Custom promise-based setTimeout implementation
-const setTimeoutPromise = (ms) => new Promise(resolve => setTimeout(resolve, ms));
-
 // Code to retrieve value from the database
 document.addEventListener('DOMContentLoaded', async function() {
-    let guestLimit = sessionStorage.getItem('guestLimit');
-    while (!guestLimit){
-        await setTimeoutPromise(1000);
-        guestLimit = sessionStorage.getItem('guestLimit')
-    }
-
-    document.getElementById("guests-count").innerText = 'Current guest limit is ' + guestLimit;
-    console.log(guestLimit)
-    const guestsMessage = document.getElementById("guests-message")
-    if (guestLimit > 0) {
-        guestsMessage.innerText = "There is space available! Please message bellbclub@gmail.com to join us this week.";
-        guestsMessage.style.color = 'green'
-    } else {
-        guestsMessage.innerText = "Unfortunately there are no more spots available. Please check again later as spots may open up.\n A total of two guests are allowed per week."
-        guestsMessage.style.color = 'red'
+    const guestLimit = sessionStorage.getItem('guestLimit');
+    if (guestLimit) {
+        document.getElementById("guests-count").innerText = 'Current guest limit is ' + guestLimit;
+        console.log(guestLimit)
+        const guestsMessage = document.getElementById("guests-message")
+        if (guestLimit > 0) {
+            guestsMessage.innerText = "There is space available! Please message bellbclub@gmail.com to join us this week.";
+            guestsMessage.style.color = 'green'
+        } else {
+            guestsMessage.innerText = "Unfortunately there are no more spots available. Please check again later as spots may open up.\n A total of two guests are allowed per week."
+            guestsMessage.style.color = 'red'
+        }
     }
 });
